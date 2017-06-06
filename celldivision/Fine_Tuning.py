6b23cb71c2088babd0a008cc9ff884158f2e11dc
PardoAlejo <la.pardo@uniandes.edu.co>
2017-06-06 12:25:49 -0500

    Data Augmentation

--- celldivision/Fine_Tuning.py
+++ celldivision/Fine_Tuning.py
@@ -107,22 +107,47 @@ opt =keras.optimizers.Adagrad(lr=0.001, epsilon=1e-08, decay=0.001)
 model.compile(loss='categorical_crossentropy',
               optimizer=opt,
               metrics=['accuracy'])
+
+if not data_augmentation:
+
+  print('Start Training')
+  start = time.time()
+  model.fit(x_train, y_train,
+            batch_size=batch_size,
+            epochs=epochs,
+            validation_data=(x_test, y_test),
+            shuffle=True)#,class_weight = class_weight)
+  end = time.time()
+  elapsed_time = end-start
+  print('Train time', elapsed_time)
+
+else:
+    print('Using real-time data augmentation.')
+    # This will do preprocessing and realtime data augmentation:
+    datagen = ImageDataGenerator(
+                                 featurewise_center=False, # set input mean to 0 over the dataset
+                                 samplewise_center=False, # set each sample mean to 0
+                                 featurewise_std_normalization=False, # divide inputs by std of the dataset
+                                 samplewise_std_normalization=False, # divide each input by its std
+                                 zca_whitening=False, # apply ZCA whitening
+                                 rotation_range=0, # randomly rotate images in the range (degrees, 0 to 180)
+                                 width_shift_range=0.1, # randomly shift images horizontally (fraction of total width)
+                                 height_shift_range=0.1, # randomly shift images vertically (fraction of total height)
+                                 horizontal_flip=True, # randomly flip images
+                                 vertical_flip=False) # randomly flip images
+    datagen.fit(x_train)
+    
+    model.fit_generator(datagen.flow(x_train, y_train,
+                        batch_size=batch_size),
+                        steps_per_epoch=x_train.shape[0] // batch_size,
+                        epochs=epochs,
+                        validation_data=(x_test, y_test))
+
 """
 class_weight = {0 : 0,
     1: 50.,
     2: 1.}
 """
-print('Start Training')
-start = time.time()
-model.fit(x_train, y_train,
-          batch_size=batch_size,
-          epochs=epochs,
-          validation_data=(x_test, y_test),
-          shuffle=True)#,class_weight = class_weight)
-end = time.time()
-elapsed_time = end-start
-print('Train time', elapsed_time)
-
 classes = model.predict(x_test, batch_size=64)
 y_pred = []
 for i in range(classes.shape[0]):
