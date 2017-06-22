a96167167b03c738ae59e43f9a91b30aef02add0
fuanka <fuankarion@gmail.com>
2017-06-21 21:13:06 -0500

    Approximate area script

--- celldivision/segmentation/areaCalculation.py
+++ celldivision/segmentation/areaCalculation.py
@@ -2,11 +2,20 @@ import cv2
 import math
 import numpy as np
 
-im = cv2.imread('/home/jcleon/DAKode/CellSegmentation/celldivision/segmentation/r216.jpg', 0)
+im = cv2.imread('./r152.jpg', 0)
+"""
+cv2.imshow('image',im)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+"""
 
 print(im.shape)
-im[im > 1] = 0 
+im[im > 1] = 1
+
+#hist, bin_edge = np.histogram(im, bins=[x for x in range(0, 260)])
+#print('hist', hist)
+
 nonZero = np.count_nonzero(im)
 print('nonZero ', nonZero)
 radius = math.sqrt(nonZero / math.pi)
-print('radius', radius)
+print('Approx radius', radius)
